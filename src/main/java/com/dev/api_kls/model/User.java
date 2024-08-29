cf21291ce784bd1601d0cf95ca5769eaa87b5a54
Elisariane <elisarianeBS@gmail.com>
2024-08-29 08:44:22 -0300

    fix: Inclui token secret e construtor na classe User

--- src/main/java/com/dev/api_kls/model/User.java
+++ src/main/java/com/dev/api_kls/model/User.java
@@ -4,8 +4,10 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -16,6 +18,8 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User implements UserDetails {
 
     @Id
